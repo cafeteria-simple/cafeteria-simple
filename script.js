f101215b023371f97ace454a6b55230b7d472fe5
cafeteria-simple <salvadorogamoran2+2@gmail.com>
2024-05-30 22:31:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function send() {
 	let Mesa = document.getElementById("mesa").value;
 	let Pedido = document.getElementById("pedido").value;
 	
-	const webhook = "https://discord.com/api/webhooks/1238925995046604810/7mt5Uak7gJwgqd0w4nmmBMXY6Id6XAi_Ka_41LWtAV8dNV8JGG4PuG2eb3FaLWFw9zc-";
+	const webhook = "https://discord.com/api/webhooks/1238926464410451999/fODRWicCcJAJFNMy-qzEUoJyjMyQgZmV55si_YeK3r_jcVaXelGWhKv4bB2Ikr3_XGvW";
 	
-	const contents = '# -------------------------------' + '\nMesa: ' + Mesa + '\nPedido: ' + Pedido + '\n# -------------------------------'; 
+	const contents = '# -------------------------------' + '\nMesa: ' + nombre + '\nPedido: ' + pago + '\n# -------------------------------'; 
 	const request = new XMLHttpRequest();
 	request.open("POST", webhook);
 	request.setRequestHeader('Content-type', 'application/json');
